87bc24099c7e201adc941a0e589fa023020eb91e
Nosa Osagiede <osagiedenosakhare@gmail.com>
2023-01-12 22:47:17 +0100

    hompage4

--- components/SendNotification.js
+++ components/SendNotification.js
@@ -7,36 +7,36 @@ export default function SendNotification({users, fcmAccessToken}){
 
     useEffect(() => {
       if (regTokens.length > 0){
-      regTokens?.map(token => {
-        const payload = {
-          message: {
-            token: token,
-            notification: {
-              title: "Chinese New year BONUS!!!",
-              body: "10% off all movies in January"
-            },
-            data: {
-                url: `/offers`
-            },
-            webpush: {
-              fcm_options: {
-                link: `/offers`
+        regTokens?.map(token => {
+          const payload = {
+            message: {
+              token: token,
+              notification: {
+                title: "Chinese New year BONUS!!!",
+                body: "10% off all movies in January"
+              },
+              data: {
+                  url: `/offers`
+              },
+              webpush: {
+                fcm_options: {
+                  link: `/offers`
+                }
               }
             }
           }
+          fetch('https://fcm.googleapis.com//v1/projects/fcm-demo-d11b4/messages:send', { 
+            method: 'POST', 
+            headers: {
+              'Authorization': 'Bearer '+ fcmAccessToken.accessToken, 
+              'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(payload)
+          }).then((res) => res.json())
+          .then((data) => (console.log(data, "token", token)));
+        })
+          // console.log("token here", token)   
         }
-        fetch('https://fcm.googleapis.com//v1/projects/fcm-demo-d11b4/messages:send', { 
-          method: 'POST', 
-          headers: {
-            'Authorization': 'Bearer '+ fcmAccessToken.accessToken, 
-            'Content-Type': 'application/json'
-          }, 
-          body: JSON.stringify(payload)
-        }).then((res) => res.json())
-        .then((data) => (console.log(data, "token", token)));
-      }).catch((e)=>console.log(e))
-        // console.log("token here", token)   
-      }
     }, [])
     return(
         <>
